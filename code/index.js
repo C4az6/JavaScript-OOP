76ac1aca7bd92688c6525c5b833f7f60de682378
c4az6 <1762458611@qq.com>
2022-12-07 12:37:35 +0800

    update

---
+++ code/index.js
@@ -0,0 +1,23 @@
+/* 
+  Class可以通过extends关键字实现继承，让子类继承父类的属性和方法
+*/
+
+/* class Point {
+
+}
+
+class ColorPoint extends Point {
+
+} */
+
+// 上面示例中，Point是父类，ColorPoint是子类，它通过extends关键字，继承了Point类的所有属性和方法。但是由于没有部署任何代码，所以这两个类完全一样，等于复制了一个Point类。
+
+
+class Point {
+
+}
+
+
+class ColorPoint extends Point {
+
+}
